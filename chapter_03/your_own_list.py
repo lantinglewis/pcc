5f69b839d6ba51cdf5890ca301bca90222f7c24d
lantinglewis <lewislanting@hotmail.com>
2022-12-04 19:25:35 +0100

    chapter 3 WIP

---
+++ chapter_03/your_own_list.py
@@ -0,0 +1,10 @@
+fav_vehicles = ['car', 'plane', 'train']
+
+msg = f"I would like to own a {fav_vehicles[0]}."
+print(msg)
+
+msg = f"I would like to own a {fav_vehicles[1]}."
+print(msg)
+
+msg = f"I would like to own a {fav_vehicles[2]}."
+print(msg)
